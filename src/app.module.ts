f172105a83bb2324e1b40187b7aa49e514137eb5
awqsomee <awqsome@bk.ru>
2022-10-27 17:57:32 +0300

    Structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import { SequelizeModule } from '@nestjs/sequelize'
+import { UsersModule } from './users/users.module'
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable, Get } from '@nestjs/common'
-
-@Injectable()
-export class AppService {
-  getUsers() {
-    return [{ id: 1, name: 'Awh' }]
-  }
-}
