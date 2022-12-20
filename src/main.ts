1aa1993a79db7690707d812ad19313219340887f
awqsomee <awqsome@bk.ru>
2022-12-20 22:20:39 +0300

    try-catch

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,24 @@ import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
-async function start() {
-  const PORT = process.env.PORT || 5000
-  const app = await NestFactory.create(AppModule)
+const start = async () => {
+  try {
+    const PORT = process.env.PORT || 5000
+    const app = await NestFactory.create(AppModule)
 
-  const config = new DocumentBuilder()
-    .setTitle('Play2GetHer')
-    .setDescription('REST API documentation')
-    .setVersion('1.0.0')
-    .addTag('awqsomee')
-    .build()
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('/api/docs', app, document)
+    const config = new DocumentBuilder()
+      .setTitle('Play2GetHer')
+      .setDescription('REST API documentation')
+      .setVersion('1.0.0')
+      .addTag('awqsomee')
+      .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('/api/docs', app, document)
 
-  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
+    await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 start()
